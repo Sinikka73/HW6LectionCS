ea7d3b2f07b34446927234e60ddb40b7a4742318
olga <bend_sinister@mail.ru>
2022-10-09 09:48:26 +0300

    T43

---
+++ Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*double getIntersectionPointCoordinates(double b1, double k1, double b2, double k2)
+{
+    if((k1 - k2) == 0)
+    {
+        Console.Write("Задача не имеет решения");
+    }
+    else
+    {
+        double coordX = (b2 - b1) / (k1 - k2);
+        double coordY = k1 * coordX + b1;
+    }
+}
+*/
+
+Console.Write("Введите значение b1: ");
+double userB1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double userK1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите значение b2: ");
+double userB2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double userK2 = Convert.ToInt32(Console.ReadLine());
+
+if((userK1 - userK2) == 0)
+    {
+        Console.Write("Задача не имеет решения");
+    }
+    else
+    {
+        double coordX = (userB2 - userB1) / (userK1 - userK2);
+        double coordY = userK1 * coordX + userB1;
+        Console.WriteLine($"Точка пересечения с координатами Х = {coordX} и Y = {coordY}");
+    }
